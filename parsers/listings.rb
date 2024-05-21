16657d3660d4956716fc3d8f3475716265f3a92e
Galang <davimuhammad@galang.local>
2024-05-22 01:20:37 +0700

    modified listings parser to enqueue more pages

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -8,13 +8,13 @@ listings = nokogiri.css('ul.b-list__items_nofooter li.s-item')
 listings.each do |listing|
     # initialize an empty hash
     product = {}
-
+    
     # extract the information into the product hash
     product['title'] = listing.at_css('h3.s-item__title')&.text
-
+    
     # extract the price
     product['price'] = listing.at_css('.s-item__price')&.text
-
+    
     # extract the listing URL
     item_link = listing.at_css('a.s-item__link')
     product['url'] = item_link['href'] unless item_link.nil?
@@ -22,6 +22,16 @@ listings.each do |listing|
     # specify the collection where this record will be stored
     product['_collection'] = "listings"
 
-    # save the product to the job’s outputs
+    # save the product to the outputs.
     outputs << product
+
+    # enqueue more pages to the scrape job
+    pages << {
+        url: product['url'],
+        page_type: 'listings',
+        vars: {  # adding vars to this page
+            title: product['title'],
+            price: product['price']
+        }
+    }
 end
