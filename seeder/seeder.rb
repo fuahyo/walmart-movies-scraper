831837709a146d11311bb2006fda224d2235b10c
Galang Dwi Juniar <galang@Galang.local>
2024-05-27 21:59:30 +0700

    first

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -8,12 +8,12 @@ pages << {
   }
   # url: "https://www.amazon.com/s?k=lcd+%26+led+tv&i=electronics&rh=n%3A1266092011&qid=1716820752&ref=sr_pg_1"
 }
-pages << {
-  page_type: 'listings',
-  method: "GET",
-  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.amazon.com/s?k=oled+tv%27s&rh=n%3A1266092011&ref=nb_sb_noss",
-  vars: {
-    category: "OLED TVs"
-  }
-}
+# pages << {
+#   page_type: 'listings',
+#   method: "GET",
+#   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+#   url: "https://www.amazon.com/s?k=oled+tv%27s&rh=n%3A1266092011&ref=nb_sb_noss",
+#   vars: {
+#     category: "OLED TVs"
+#   }
+# }
