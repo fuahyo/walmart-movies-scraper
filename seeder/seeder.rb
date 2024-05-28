26c6887c990d0e09378e7384a86ce70a7a4ec52c
Galang Dwi Juniar <galang@Galang.local>
2024-05-28 22:01:48 +0700

    first

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,18 +2,7 @@ pages << {
   page_type: 'listings',
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.amazon.com/s?k=lcd+%26+led+tv&rh=n%3A1266092011&ref=nb_sb_noss",
-  vars: {
-    category: "LED & LCD TVs"
-  }
-  # url: "https://www.amazon.com/s?k=lcd+%26+led+tv&i=electronics&rh=n%3A1266092011&qid=1716820752&ref=sr_pg_1"
-}
-pages << {
-  page_type: 'listings',
-  method: "GET",
-  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.amazon.com/s?k=oled+tv%27s&rh=n%3A1266092011&ref=nb_sb_noss",
-  vars: {
-    category: "OLED TVs"
-  }
+  url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+  fetch_type: "browser",
+  force_fetch: true
 }
